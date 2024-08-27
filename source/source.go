eb92e908f6e6352d5f825d4211e6ee8dcc5cafba
hantmac <hantmac@outlook.com>
2024-08-27 12:01:35 +0800

    debug

--- source/source.go
+++ source/source.go
@@ -348,11 +348,13 @@ func (s *Source) GetDatabasesAccordingToSourceDbRegex(sourceDatabasePattern stri
 		if err != nil {
 			return nil, err
 		}
+		fmt.Println("sourcedatabase pattern", sourceDatabasePattern)
 		match, err := regexp.MatchString(sourceDatabasePattern, database)
 		if err != nil {
 			return nil, err
 		}
 		if match {
+			fmt.Println("match db: ", database)
 			databases = append(databases, database)
 		}
 	}
